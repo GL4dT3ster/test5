e2f868c30554f1115145303647f477f933bbf41c
GL4dT3ster <gl4dt3ster@users.noreply.github.com>
2024-06-08 12:36:56 +0400

    .

--- src/main.java
+++ src/main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
